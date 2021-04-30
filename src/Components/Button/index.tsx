933da1dfd5e12af8d7596ac08eab30091813e684
Nick Carter <nick@forethought.ai>
2021-04-29 17:54:35 -0700

    oops i did it again

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,1 +1,26 @@
-export { };
+import React from 'react';
+import 'my-Btn-Styles.css';
+
+const Button = (myProps) => {
+    const { btnTxt, btnLnk, btnCls, myfunction} = myProps;
+    const clicky = () => {
+        myfunction()
+    }
+    return (
+        <div 
+            onClick={() => clicky()} 
+            className='container' 
+            style={{'border':'1px'}}>
+            <button 
+                className='btn' 
+                style={{'display':'flex', 'justifyContent':'center', 'alignItems':'center'}}>
+                <span 
+                    className='text' 
+                    style={{'fontSize':'14px', 'color':'black'}}>
+                    {btnTxt}
+                </span>
+            </button>
+        </div>
+    )
+}
+export default Button;
